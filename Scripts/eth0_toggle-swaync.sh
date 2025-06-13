5acbf4f56fd02b9ff76d2fd8b07415e78aa0099d
fleshguard <168491129+fleshguard@users.noreply.github.com>
2025-06-13 19:28:22 -0400

    Delete Scripts directory

--- Scripts/eth0_toggle-swaync.sh
+++
@@ -1,9 +0,0 @@
-eth0_state=$(nmcli device show eth0 | awk -F': +' '/GENERAL.STATE/ {print $2}')
-
-if [[ "$eth0_state" == '100 (connected)' ]]; then
-  nmcli device down eth0 
-  echo 'eth0 down'
-else
-  nmcli device up eth0 
-  echo 'eth0 up'
-fi
